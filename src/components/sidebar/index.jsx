6651c7d755fdbcf0accddfd25ec6689906ea923b
Sujosh <sujoshk@gmail.com>
2020-06-26 13:51:40 +0530

    misc

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -5,21 +5,62 @@ import Card from '../../components/UI/card';
 const Sidebar = (props) => {
   return(
     <div className="sidebar-container">
-        <Card style={{marginBottom: "20px"}}>
+        
+        
+        <Card style={{marginBottom: "10px"}}>
             <div className="card-header">
                 <span>About us</span>
-                <div className="profile-pic">
+                <div className="profile-image-container">
                     <img src="https://restaurateurs.goodfrance.com/sites/default/files/Sujosh%20Profile%20Pic.jpg" alt="profile image"/>
 
                 </div>
+            </div>
+
+            <div  className="card-body">
+                <p className="personal-bio">
+                    My name is Sujosh Kahali
+                </p>
             </div>            
         </Card>     
 
-        <Card>
+        <Card style={{marginBottom: "10px"}}>
             <div className="card-header">
                 <span>Social Network</span>
             </div>            
         </Card>    
+
+
+        <Card style={{marginBottom: "10px"}}>
+            <div className="card-header">
+                <span>Recent Posts</span>
+            </div>
+
+            <div className="recent-posts">
+                
+               {/* Recent post 1 */}
+                <div className="recent-post">
+                    <h3>How to not be</h3>
+                    <span>June 29, 2018</span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, minima...</p>
+                </div>
+
+                {/* Recent post 2 */}
+                <div className="recent-post">
+                    <h3>How not to do</h3>
+                    <span>June 29, 2018</span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, minima...</p>
+                </div>
+
+                {/* Recent post 3 */}
+                <div className="recent-post">
+                    <h3>How not to not</h3>
+                    <span>June 29, 2018</span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus, minima...</p>
+                </div>
+
+            </div>
+
+        </Card>
     </div>       
    )
   }
